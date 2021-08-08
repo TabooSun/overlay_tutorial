4eb8fb848c31e2afb48e29420e729e5d471d4bef
MrCsabaToth <csaba.toth.us@gmail.com>
2021-08-07 20:00:22 -0700

    Enhance the example app to include absorbpointer and also make the tutorial popup on command (question mark action button) and could be dismissed.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,12 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Overlay Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Overlay Demo'),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -36,6 +36,8 @@ class _MyHomePageState extends State<MyHomePage>
   Animation<Offset> _tweenAnimation;
 
   int _counter = 0;
+  bool _tutorialVisible = false;
+  bool _absorbPointer = true;
 
   void _incrementCounter() {
     setState(() {
@@ -64,63 +66,31 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final tutorialColor = Colors.yellow;
-    return OverlayTutorialScope(
-      enabled: true,
-      absorbPointer: true,
-      overlayColor: Colors.blueAccent.withOpacity(.6),
-      child: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                AnimatedBuilder(
-                  animation: _tweenAnimation,
-                  builder: (context, child) {
-                    return SlideTransition(
-                      position: _tweenAnimation,
-                      child: child,
-                    );
-                  },
-                  child: OverlayTutorialHole(
-                    enabled: true,
-                    overlayTutorialEntry: OverlayTutorialCustomShapeEntry(
-                      shapeBuilder: (rect, path) {
-                        path = Path.combine(
-                          PathOperation.difference,
-                          path,
-                          Path()
-                            ..addOval(Rect.fromLTWH(
-                              rect.left - 16,
-                              rect.top,
-                              112,
-                              64,
-                            )),
-                        );
-                        return path;
-                      },
-                    ),
-                    child: Icon(
-                      Icons.share,
-                      size: 64,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 64),
-                Text(
-                  'You have pushed the button this many times:',
-                ),
+    return GestureDetector(
+      onTap: _tutorialVisible
+          ? () {
+              setState(() {
+                _tutorialVisible = false;
+              });
+            }
+          : null,
+      child: OverlayTutorialScope(
+        enabled: _tutorialVisible,
+        absorbPointer: _absorbPointer,
+        overlayColor: Colors.green.withOpacity(.6),
+        child: SafeArea(
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: [
                 OverlayTutorialHole(
-                  enabled: true,
+                  enabled: _tutorialVisible,
                   overlayTutorialEntry: OverlayTutorialRectEntry(
                     padding: const EdgeInsets.symmetric(horizontal: 8.0),
                     radius: const Radius.circular(16.0),
                     overlayTutorialHints: <OverlayTutorialWidgetHint>[
                       OverlayTutorialWidgetHint(
-                        position: (rect) => Offset(0, rect.bottom),
+                        position: (rect) => Offset(rect.center.dx / 6, rect.center.dy / 1.5),
                         builder: (context, rect, rRect) {
                           return SizedBox(
                             width: MediaQuery.of(context).size.width,
@@ -128,9 +98,9 @@ class _MyHomePageState extends State<MyHomePage>
                               child: Padding(
                                 padding: const EdgeInsets.all(8.0),
                                 child: Text(
-                                  'Current Counter will be displayed here',
-                                  style: textTheme.bodyText2
-                                      .copyWith(color: tutorialColor),
+                                    'Turn on tutorial',
+                                    style: textTheme.bodyText2
+                                        .copyWith(color: tutorialColor),
                                 ),
                               ),
                             ),
@@ -139,64 +109,159 @@ class _MyHomePageState extends State<MyHomePage>
                       ),
                     ],
                   ),
-                  child: Text(
-                    '$_counter',
-                    style: Theme.of(context).textTheme.headline4,
+                  child: IconButton(icon: Icon(Icons.info_rounded),
+                    onPressed: () {
+                      setState(() {
+                        _tutorialVisible = !_tutorialVisible;
+                      });
+                    },
                   ),
                 ),
               ],
             ),
-          ),
-          floatingActionButton: OverlayTutorialHole(
-            enabled: true,
-            overlayTutorialEntry: OverlayTutorialRectEntry(
-              padding: const EdgeInsets.all(8.0),
-              radius: const Radius.circular(16.0),
-              overlayTutorialHints: <OverlayTutorialWidgetHint>[
-                OverlayTutorialWidgetHint(
-                  builder: (context, rect, rRect) {
-                    return Positioned(
-                      top: rRect.top - 24.0,
-                      left: rRect.left,
-                      child: Text(
-                        'Try this out',
-                        style:
-                            textTheme.bodyText2.copyWith(color: tutorialColor),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  AnimatedBuilder(
+                    animation: _tweenAnimation,
+                    builder: (context, child) {
+                      return SlideTransition(
+                        position: _tweenAnimation,
+                        child: child,
+                      );
+                    },
+                    child: OverlayTutorialHole(
+                      enabled: _tutorialVisible,
+                      overlayTutorialEntry: OverlayTutorialCustomShapeEntry(
+                        shapeBuilder: (rect, path) {
+                          path = Path.combine(
+                            PathOperation.difference,
+                            path,
+                            Path()
+                              ..addOval(Rect.fromLTWH(
+                                rect.left - 16,
+                                rect.top,
+                                112,
+                                64,
+                              )),
+                          );
+                          return path;
+                        },
                       ),
-                    );
-                  },
-                ),
-                OverlayTutorialWidgetHint(
-                  position: (rect) => Offset(0, rect.center.dy),
-                  builder: (context, rect, rRect) {
-                    return SizedBox(
-                      width: rRect.left,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                'Click here to add counter',
-                                style: textTheme.bodyText2
-                                    .copyWith(color: tutorialColor),
+                      child: Icon(
+                        Icons.share,
+                        size: 64,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 64),
+                  Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  OverlayTutorialHole(
+                    enabled: _tutorialVisible,
+                    overlayTutorialEntry: OverlayTutorialRectEntry(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      radius: const Radius.circular(16.0),
+                      overlayTutorialHints: <OverlayTutorialWidgetHint>[
+                        OverlayTutorialWidgetHint(
+                          position: (rect) => Offset(0, rect.bottom),
+                          builder: (context, rect, rRect) {
+                            return SizedBox(
+                              width: MediaQuery.of(context).size.width,
+                              child: Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    'Current Counter will be displayed here',
+                                    style: textTheme.bodyText2
+                                        .copyWith(color: tutorialColor),
+                                  ),
+                                ),
                               ),
-                            ),
-                          ],
+                            );
+                          },
                         ),
+                      ],
+                    ),
+                    child: Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                  const SizedBox(height: 64),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Checkbox(
+                        value: _absorbPointer,
+                        onChanged: (bool value) {
+                          setState(() {
+                            _absorbPointer = value;
+                          });
+                        },
                       ),
-                    );
-                  },
-                ),
-              ],
+                      Text(
+                        'Absorb Pointer',
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            child: FloatingActionButton(
-              onPressed: _incrementCounter,
-              tooltip: 'Increment',
-              child: Icon(Icons.add),
-            ),
-          ), // This trailing comma makes auto-formatting nicer for build methods.
+            floatingActionButton: OverlayTutorialHole(
+              enabled: _tutorialVisible,
+              overlayTutorialEntry: OverlayTutorialRectEntry(
+                padding: const EdgeInsets.all(8.0),
+                radius: const Radius.circular(16.0),
+                overlayTutorialHints: <OverlayTutorialWidgetHint>[
+                  OverlayTutorialWidgetHint(
+                    builder: (context, rect, rRect) {
+                      return Positioned(
+                        top: rRect.top - 24.0,
+                        left: rRect.left,
+                        child: Text(
+                          'Try this out',
+                          style: textTheme.bodyText2
+                              .copyWith(color: tutorialColor),
+                        ),
+                      );
+                    },
+                  ),
+                  OverlayTutorialWidgetHint(
+                    position: (rect) => Offset(0, rect.center.dy),
+                    builder: (context, rect, rRect) {
+                      return SizedBox(
+                        width: rRect.left,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  'Click here to add counter',
+                                  style: textTheme.bodyText2
+                                      .copyWith(color: tutorialColor),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+              child: FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: Icon(Icons.add),
+              ),
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
         ),
       ),
     );
